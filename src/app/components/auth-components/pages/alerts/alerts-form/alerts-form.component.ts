aa1a30b1b4341faaa751d25313e4b1e1cbf3b40f
ARSLAN\Arslan <m.arslan2037@gmail.com>
2020-07-24 05:34:53 +0500

    more forms are added

--- src/app/components/auth-components/pages/alerts/alerts-form/alerts-form.component.ts
+++ src/app/components/auth-components/pages/alerts/alerts-form/alerts-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
     selector: 'alerts-form',
@@ -8,10 +9,46 @@ import { Component } from '@angular/core';
 
 export class AlertsFormComponent {
 
-    services:any = [];
-    creating_service:boolean = false;
+    form:any;
+    loading:boolean = false;
+    submitted:boolean = false;
+    alert_type_list:any = [
+        {id: 1, name: 'Lost Pet', value: 'lost_pet'},
+        {id: 2, name: 'Found Pet', value: 'found_pet'},
+    ]
 
-    DisplayServicesForm() {
-        this.creating_service = true;
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit() {
+        this.CreateForm();
+        this.FormValueChanges();
+    }
+    
+    CreateForm() {
+        this.form = this.formBuilder.group({
+            'alert_type': ['undefined', Validators.required],
+            'location': ['', Validators.required],
+            'address': ['', Validators.required],
+            'date': ['', Validators.required],
+            'time': ['', Validators.required],
+            'profile_link': ['', ],
+            'detail_situation': ['', Validators.required],
+            'pet_avatar': ['', Validators.required],
+        })
+    }
+
+    FormValueChanges() {
+        // this.form.controls['free'].valueChanges.subscribe((value:any) => {
+        //     (value) ? this.form.get('price').disable() : this.form.get('price').enable();
+        //     // (value) ? this.form.get('pet_price_unit').disable() : this.form.get('pet_price_unit').enable();
+        // });
+    }
+
+    SubmitData() {
+        this.submitted = true;
+        if(this.form.valid) {
+            let data = this.form.value;
+            this.loading = true;
+        }
     }
 }

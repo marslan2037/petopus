3f1160d6003ca03ad825cba1117e91d7bba88bce
Farukh Sultan <farukh.5937@gmail.com>
2020-12-12 12:25:27 +0500

    Fixes: Blank about form component

---
+++ src/app/components/auth-components/pages/about/about-form/about-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about-form',
+  templateUrl: './about-form.component.html',
+  styleUrls: ['./about-form.component.scss']
+})
+
+export class AboutFormComponent {
+    
+}
